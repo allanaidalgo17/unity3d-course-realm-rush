685b872dc1449a1e0c6676de5050b3174910a6f4
Allana Idalgo <allanaidalgo17@gmail.com>
2022-01-12 00:04:40 -0300

    added pathfinder scripts using BFS algorithm

--- Assets/Scripts/Enemy/EnemyMover.cs
+++ Assets/Scripts/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] List<Tile> path = new List<Tile>();
     [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     private Enemy enemy;
@@ -30,7 +30,7 @@ public class EnemyMover : MonoBehaviour
 
         foreach (Transform pathChild in pathParent.transform)
         {
-            Waypoint waypoint = pathChild.GetComponent<Waypoint>();
+            Tile waypoint = pathChild.GetComponent<Tile>();
 
             if (waypoint != null)
             {
@@ -52,7 +52,7 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator PrintWaypointName()
     {
-        foreach (Waypoint waypoint in path)
+        foreach (Tile waypoint in path)
         {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
--- Assets/Scripts/PathFinding/GridManager.cs
+++ Assets/Scripts/PathFinding/GridManager.cs
@@ -6,7 +6,13 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
 
+    [Tooltip("Unity Grid Size - Should match UnityEditor snap settings.")]
+    [SerializeField] int unityGridSize = 10;
+
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    public Dictionary<Vector2Int, Node> Grid { get => grid; }
+    public int UnityGridSize { get => unityGridSize; }
 
     private void Awake()
     {
@@ -15,8 +21,44 @@ public class GridManager : MonoBehaviour
 
     public Node GetNode(Vector2Int key)
     {
-        if (!grid.ContainsKey(key)) return null;
-        return grid[key];
+        if (!Grid.ContainsKey(key)) { return null; }
+        return Grid[key];
+    }
+
+    public void BlockNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates))
+        {
+            grid[coordinates].isWalkable = false;
+        }
+    }
+
+    public void ResetNodes()
+    {
+        foreach (KeyValuePair<Vector2Int, Node> entry in grid)
+        {
+            entry.Value.connectedTo = null;
+            entry.Value.isPath = false;
+            entry.Value.isExplored = false;
+        }
+    }
+
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / unityGridSize);
+        coordinates.y = Mathf.RoundToInt(position.z / unityGridSize);
+
+        return coordinates;
+    }
+
+    public Vector3 GetPositionFromCoordinates(Vector2Int coordinates)
+    {
+        Vector3 position = new Vector3();
+        position.x = coordinates.x * unityGridSize;
+        position.z = coordinates.y * unityGridSize;
+
+        return position;
     }
 
     private void CreateGrid()
@@ -27,7 +69,7 @@ public class GridManager : MonoBehaviour
             {
                 Vector2Int key = new Vector2Int(x, y);
                 Node value = new Node(key, true);
-                grid.Add(key, value);
+                Grid.Add(key, value);
             }
         }
     }
